ee298e94c84d5d240e15ab82fa2c7cf6061076c7
Facundo Lucero <facundon.lucero@alumnos.frm.utn.edu.ar>
2022-08-03 18:03:01 -0300

    first

---
+++ src/main/java/com/example/testingautomatico/TestingAutomaticoApplication.java
@@ -0,0 +1,13 @@
+package com.example.testingautomatico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingAutomaticoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingAutomaticoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testingautomatico/TestingAutomaticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testingautomatico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingAutomaticoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
